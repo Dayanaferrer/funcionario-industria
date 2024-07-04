b26a8569cd58553ecae3edbb7f351956c7b64c03
Dayanaferrer <dayanaferreira2@gmail.com>
2024-07-04 18:18:50 -0300

    Iniciando sistema com config conexao banco de dados e arquivos ignore

---
+++ industria/src/test/java/com/sistema/industria/IndustriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.industria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
